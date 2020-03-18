3649a427cefcec781b6a7a474fc27bcaa1381915
Osvaldo González Venegas <ojgonzalez@falabella.cl>
2020-03-17 23:54:22 -0300

    fixing inital server config

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const server = require('./server');
-const {PORT} = require('./config');
+const {PORT, MONGO_URI} = require('./config');
+const mongoose = require('mongoose');
 
-server.listen(PORT, () => {
-    console.log(`codingApp listening on port ${PORT}`);
-});
+mongoose.connect(MONGO_URI, {useNewUrlParser: true})
+.then( () => {
+    server.listen(PORT, () => {
+        console.log(`codingApp listening on port ${PORT}`);
+    });
+})
+.catch(console.log);
